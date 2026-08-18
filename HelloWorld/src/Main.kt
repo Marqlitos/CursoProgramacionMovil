5fb4c6deaaa9d0c6578eaf634f64f344b70104f6
Marqlitos <131511588+Marqlitos@users.noreply.github.com>
2026-08-17 21:32:39 -0600

    Add files via upload

--- HelloWorld/src/Main.kt
+++ HelloWorld/src/Main.kt
@@ -1,6 +1,7 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Hello World!")
-
+    val nombre = "Marco"
+    val edad = 22
+    print("Mi nombre es: $nombre y tengo $edad ")
 }

62faa0ab0743096744ee8d0f50ac0d9181e6f15c
Marqlitos <131511588+Marqlitos@users.noreply.github.com>
2026-08-17 21:42:49 -0600

    Add files via upload

---
+++ TiposBasicos/src/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val a : Int = 1000
+    val b : String = "log message"
+    val c : Double = 3.14
+    val d : Long = 100_000_000_000_000
+    val e : Boolean = false
+    val f : Char = '\n'
+
+    print("$a, $b $c $d $e $f \n")
+}
